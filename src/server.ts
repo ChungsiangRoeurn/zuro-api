a4a55eb96b230bb77d61e99cc020ef9090ee6f78
Chungsiang Roeurn <mrchungsiang10@gmail.com>
2026-03-02 17:10:47 +0700

    fix:feat: update API version to 1.0 and configure Vercel deployment

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,27 @@
 import "dotenv/config";
 import express from "express";
 import { MainRouter } from "./routers/index.ts";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// =========== Serve public folder ===========
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "../public")));
+
+// =========== Root Router ===========
 app.get("/", (req, res) => {
-  res.send("Hello from API");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+// =========== Main Router ===========
 app.use("/api", MainRouter);
-
-const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on 🗿 http://localhost:${PORT}`);
